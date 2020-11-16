57842d780cf162a570e0dab2b27f5c8b5c02739c
Your Name <you@example.com>
2020-11-16 15:13:13 -0600

    finished challenge

---
+++ PalindromeExercise/PalindromeExercise/Palindrome.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            //reverse the word
+            var reversed = "";
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed += word[i];
+            }
+            //compare with original
+
+            if (reversed.ToLower() == word.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            //return if they are same or not
+        }
+        
+
+    }
+
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,23 @@
 using System;
+using PalindromeExercise;
 using Xunit;
+ 
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("Racecar", true)]
+        public void Test1(string word, bool expected)
         {
+            var tester = new WordSmith();
 
+            var actual = tester.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
